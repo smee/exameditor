1d4d8ada59011b4ae30232865c7232a2f11e8468
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2009-09-18 17:13:36 +0200

    removed buggy css menu dependency, was missing all resources...

--- exameditor/web/src/main/java/de/elateportal/editor/pages/OverviewPage.java
+++ exameditor/web/src/main/java/de/elateportal/editor/pages/OverviewPage.java
@@ -1,18 +1,12 @@
 package de.elateportal.editor.pages;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.model.Model;
-import org.codesmell.wicket.cssmenu.Constants;
-import org.codesmell.wicket.cssmenu.LabeledMenuItem;
-import org.codesmell.wicket.cssmenu.MenuItem;
-import org.codesmell.wicket.cssmenu.quickmenu.dropshadow.QuickMenu;
 
+import de.elateportal.editor.components.menu.ChromeMenu;
 import de.elateportal.editor.components.menu.LinkVO;
 import de.elateportal.editor.components.menu.LinkVO.Create;
 import de.thorstenberger.taskmodel.complex.complextaskdef.ClozeSubTaskDef;
@@ -27,8 +21,7 @@ public class OverviewPage extends WebPage {
     public OverviewPage() {
         // add(new DataStyleLink("css"));
 
-        // add(new ChromeMenu("menubar", getMenuList(), ChromeMenu.Theme.THEME1));
-        add(new QuickMenu("menubar", getQuickMenuItemsList()));
+        add(new ChromeMenu("menubar", getMenuList(), ChromeMenu.Theme.THEME1));
     }
 
     /**
@@ -67,42 +60,5 @@ public class OverviewPage extends WebPage {
         res.add(Arrays.asList(new LinkVO(StatisticPage.class, "Statistiken")));
         res.add(Arrays.asList(new LinkVO(UploadComplexTaskdefPage.class, "Importieren")));
         return res;
-    }
-
-    private List<MenuItem> getQuickMenuItemsList() {
-        final List<MenuItem> items = new ArrayList<MenuItem>();
-        final MenuItem tl1 = new LabeledMenuItem(new Link(Constants.LINK_ID) {
-
-            @Override
-            public void onClick() {
-                setResponsePage(ShowSubtaskDefsPage.class);
-                }
-                        }, Model.of("Alle Aufgaben"));
-        final MenuItem tl2 = new LabeledMenuItem(new Link(Constants.LINK_ID) {
-
-            @Override
-            public void onClick() {
-                setResponsePage(StatisticPage.class);
-                }
-                        }, Model.of("Statistik"));
-        final MenuItem tl3 = new LabeledMenuItem(new Link(Constants.LINK_ID) {
-
-            @Override
-            public void onClick() {
-                setResponsePage(UploadComplexTaskdefPage.class);
-                }
-                        }, Model.of("Statistik"));
-
-        tl1.addMenuItem(new LabeledMenuItem(new Link(Constants.LINK_ID) {
-
-            @Override
-            public void onClick() {
-                setResponsePage(new ShowSubtaskDefsPage(McSubTaskDef.class));
-                }
-                        }, Model.of("Alle MC-Aufgaben")));
-        items.add(tl1);
-        items.add(tl2);
-        items.add(tl3);
-        return items;
     }
 }
