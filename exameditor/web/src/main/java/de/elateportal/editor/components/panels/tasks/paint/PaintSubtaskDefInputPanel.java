5d92a1587d877cd398e7625dd0a5b00ca84dbf37
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2009-11-04 17:39:52 +0100

    added link reference

--- exameditor/web/src/main/java/de/elateportal/editor/components/panels/tasks/paint/PaintSubtaskDefInputPanel.java
+++ exameditor/web/src/main/java/de/elateportal/editor/components/panels/tasks/paint/PaintSubtaskDefInputPanel.java
@@ -10,6 +10,7 @@ public class PaintSubtaskDefInputPanel extends Panel {
 
     public PaintSubtaskDefInputPanel(final String id) {
         super(id);
+        // http://mrhaki.blogspot.com/2009/05/wicket-component-for-java-deployment.html
         add(new TextArea<TextSubTaskDef>("initialTextFieldValue"));
         add(new TextField<TextSubTaskDef>("textFieldHeight"));
         add(new TextField<TextSubTaskDef>("textFieldWidth"));
