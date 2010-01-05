90bac5533bd718e75b88bb8ceba1000a1ffcbd2c
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2010-01-05 17:12:23 +0100

    [feature] highlight selected tree node

--- exameditor/web/src/main/java/de/elateportal/editor/components/panels/tree/ComplexTaskDefTree.java
+++ exameditor/web/src/main/java/de/elateportal/editor/components/panels/tree/ComplexTaskDefTree.java
@@ -21,6 +21,7 @@ package de.elateportal.editor.components.panels.tree;
 import java.util.Iterator;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.CSSPackageResource;
 import org.apache.wicket.markup.html.resources.CompressedResourceReference;
 import org.apache.wicket.model.IModel;
@@ -35,40 +36,80 @@ import de.elateportal.model.ComplexTaskDef;
  */
 public class ComplexTaskDefTree extends NestedTree {
 
-	private final TaskDefPage taskDefPage;
-	private ComplexTaskDef currentTaskdef;
+    private final TaskDefPage taskDefPage;
+    private ComplexTaskDef currentTaskdef;
+    private IModel<?> selectedModel;
 
-	public ComplexTaskDefTree(String id, TaskDefPage taskDefPage, ComplexTaskdefTreeProvider provider) {
-		super(id, provider);
-		this.taskDefPage = taskDefPage;
-		add(CSSPackageResource.getHeaderContribution(new CompressedResourceReference(ComplexTaskDefTree.class, "theme/theme.css")));
+    public ComplexTaskDefTree(final String id, final TaskDefPage taskDefPage, final ComplexTaskdefTreeProvider provider) {
+        super(id, provider);
+        this.taskDefPage = taskDefPage;
+        add(CSSPackageResource.getHeaderContribution(new CompressedResourceReference(ComplexTaskDefTree.class, "theme/theme.css")));
 
-		currentTaskdef = selectFirstTaskdef(provider);
-	}
+        currentTaskdef = selectFirstTaskdef(provider);
+    }
 
-	public ComplexTaskDef getCurrentTaskdef() {
-		return currentTaskdef;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.wicket.Component#detachModels()
+     */
+    @Override
+    public void detachModels() {
+        super.detachModels();
+        if (selectedModel != null) {
+            selectedModel.detach();
+        }
+    }
+    public ComplexTaskDef getCurrentTaskdef() {
+        return currentTaskdef;
+    }
 
-	@Override
-	protected Component newContentComponent(String id, IModel model) {
-		return new TaskTreeElement(id, this, taskDefPage, model);
-	}
+    @Override
+    protected Component newContentComponent(final String id, final IModel model) {
+        return new TaskTreeElement(id, this, taskDefPage, model);
+    }
 
-	/**
-	 * @param provider
-	 * @return
-	 */
-	private ComplexTaskDef selectFirstTaskdef(ComplexTaskdefTreeProvider provider) {
-		Iterator<? extends Object> roots = provider.getRoots();
-		if (roots.hasNext())
-			return (ComplexTaskDef) roots.next();
-		else
-			return null;
-	}
+    /**
+     * @param provider
+     * @return
+     */
+    private ComplexTaskDef selectFirstTaskdef(final ComplexTaskdefTreeProvider provider) {
+        final Iterator<? extends Object> roots = provider.getRoots();
+        if (roots.hasNext()) {
+            return (ComplexTaskDef) roots.next();
+        } else {
+            return null;
+        }
+    }
 
-	public void setCurrentTaskdef(ComplexTaskDef t) {
-		this.currentTaskdef = t;
-	}
+    public void setCurrentTaskdef(final ComplexTaskDef t) {
+        this.currentTaskdef = t;
+    }
+
+    /**
+     * @param modelObject
+     * @param tree2
+     * @param target
+     */
+    void select(final IModel<?> modelObject, final AjaxRequestTarget target) {
+        if (selectedModel != null) {
+            // redraw the now deselected node
+            updateNode(selectedModel.getObject(), target);
+            selectedModel.detach();
+            selectedModel = null;
+        }
+        selectedModel = modelObject;
+
+        updateNode(modelObject.getObject(), target);
+    }
+
+    /**
+     * Return model of the currently selected node.
+     * 
+     * @return
+     */
+    public IModel<?> getSelected() {
+        return selectedModel;
+    }
 
 }
--- exameditor/web/src/main/java/de/elateportal/editor/components/panels/tree/ComplexTaskdefTreeProvider.java
+++ exameditor/web/src/main/java/de/elateportal/editor/components/panels/tree/ComplexTaskdefTreeProvider.java
@@ -45,115 +45,125 @@ import de.elateportal.model.ComplexTaskDef.Category.TextTaskBlock;
  * 
  */
 public class ComplexTaskdefTreeProvider implements ITreeProvider<Object> {
-	final static Function<Item, Object> itemGetter = new Function<Item, Object>() {
+    final static Function<Item, Object> itemGetter = new Function<Item, Object>() {
 
-		public Object apply(Item item) {
-			return item.getItem();
-		}
-	};
+        public Object apply(final Item item) {
+            return item.getItem();
+        }
+    };
 
-	private final IModel<List<ComplexTaskDef>> model;
+    private final IModel<List<ComplexTaskDef>> model;
 
-	public ComplexTaskdefTreeProvider(IModel<List<ComplexTaskDef>> model) {
-		this.model = model;
-	}
+    public ComplexTaskdefTreeProvider(final IModel<List<ComplexTaskDef>> model) {
+        this.model = model;
+    }
 
-	public IModel createLabelModel(IModel<?> model) {
-		return model;
-	}
+    public IModel createLabelModel(final IModel<?> model) {
+        return model;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.wicket.model.IDetachable#detach()
-	 */
-	public void detach() {
-		model.detach();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.wicket.model.IDetachable#detach()
+     */
+    public void detach() {
+        model.detach();
+    }
 
-	private Iterator<TaskBlockType> getChildren(Category cat) {
-		return Iterators.transform(cat.getMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItems().iterator(),
-		    new Function<McTaskBlockOrClozeTaskBlockOrTextTaskBlockItem, TaskBlockType>() {
+    private Iterator<TaskBlockType> getChildren(final Category cat) {
+        return Iterators.transform(cat.getMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItems().iterator(),
+                new Function<McTaskBlockOrClozeTaskBlockOrTextTaskBlockItem, TaskBlockType>() {
 
-			public TaskBlockType apply(McTaskBlockOrClozeTaskBlockOrTextTaskBlockItem block) {
-				return block.getItem();
-			}
-		});
-	}
+            public TaskBlockType apply(final McTaskBlockOrClozeTaskBlockOrTextTaskBlockItem block) {
+                return block.getItem();
+            }
+        });
+    }
 
-	private Iterator<Object> getChildren(ClozeTaskBlock tb) {
-		return Iterators.transform(tb.getClozeSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+    private Iterator<Object> getChildren(final ClozeTaskBlock tb) {
+        return Iterators.transform(tb.getClozeSubTaskDefOrChoiceItems().iterator(), itemGetter);
+    }
 
-	private Iterator<Category> getChildren(ComplexTaskDef ctd) {
-		return ctd.getCategory().iterator();
-	}
+    private Iterator<Category> getChildren(final ComplexTaskDef ctd) {
+        return ctd.getCategory().iterator();
+    }
 
-	private Iterator<Object> getChildren(MappingTaskBlock tb) {
-		return Iterators.transform(tb.getMappingSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+    private Iterator<Object> getChildren(final MappingTaskBlock tb) {
+        return Iterators.transform(tb.getMappingSubTaskDefOrChoiceItems().iterator(), itemGetter);
+    }
 
-	private Iterator<Object> getChildren(McTaskBlock tb) {
-		return Iterators.transform(tb.getMcSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+    private Iterator<Object> getChildren(final McTaskBlock tb) {
+        return Iterators.transform(tb.getMcSubTaskDefOrChoiceItems().iterator(), itemGetter);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see wickettree.ITreeProvider#getChildren(java.lang.Object)
-	 */
-	public Iterator<? extends Object> getChildren(Object object) {
-		if (object instanceof ComplexTaskDef)
-			return getChildren((ComplexTaskDef) object);
-		else if (object instanceof Category)
-			return getChildren((Category) object);
-		else if (object instanceof McTaskBlock)
-			return getChildren((McTaskBlock) object);
-		else if (object instanceof ClozeTaskBlock)
-			return getChildren((ClozeTaskBlock) object);
-		else if (object instanceof MappingTaskBlock)
-			return getChildren((MappingTaskBlock) object);
-		else if (object instanceof TextTaskBlock)
-			return getChildren((TextTaskBlock) object);
-		else if (object instanceof PaintTaskBlock)
-			return getChildren((PaintTaskBlock) object);
-		else
-			return Iterators.emptyIterator();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see wickettree.ITreeProvider#getChildren(java.lang.Object)
+     */
+    public Iterator<? extends Object> getChildren(final Object object) {
+        if (object instanceof ComplexTaskDef) {
+            return getChildren((ComplexTaskDef) object);
+        } else if (object instanceof Category) {
+            return getChildren((Category) object);
+        } else if (object instanceof McTaskBlock) {
+            return getChildren((McTaskBlock) object);
+        } else if (object instanceof ClozeTaskBlock) {
+            return getChildren((ClozeTaskBlock) object);
+        } else if (object instanceof MappingTaskBlock) {
+            return getChildren((MappingTaskBlock) object);
+        } else if (object instanceof TextTaskBlock) {
+            return getChildren((TextTaskBlock) object);
+        } else if (object instanceof PaintTaskBlock) {
+            return getChildren((PaintTaskBlock) object);
+        } else {
+            return Iterators.emptyIterator();
+        }
+    }
 
-	private Iterator<Object> getChildren(PaintTaskBlock tb) {
-		return Iterators.transform(tb.getPaintSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+    private Iterator<Object> getChildren(final PaintTaskBlock tb) {
+        return Iterators.transform(tb.getPaintSubTaskDefOrChoiceItems().iterator(), itemGetter);
+    }
 
-	private Iterator<Object> getChildren(TextTaskBlock tb) {
-		return Iterators.transform(tb.getTextSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+    private Iterator<Object> getChildren(final TextTaskBlock tb) {
+        return Iterators.transform(tb.getTextSubTaskDefOrChoiceItems().iterator(), itemGetter);
+    }
 
-	public Iterator<? extends Object> getRoots() {
-		return model.getObject().iterator();
-	}
+    public Iterator<? extends Object> getRoots() {
+        return model.getObject().iterator();
+    }
 
-	public boolean hasChildren(Object object) {
-		return Iterators.size(getChildren(object)) > 0;
-	}
+    public boolean hasChildren(final Object object) {
+        return Iterators.size(getChildren(object)) > 0;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see wickettree.ITreeProvider#model(java.lang.Object)
-	 */
-	public IModel<Object> model(Object object) {
-		return new CompoundPropertyModel<Object>(object) {
-			@Override
-			public boolean equals(Object obj) {
-				return getObject().equals(((IModel) obj).getObject());
-			}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see wickettree.ITreeProvider#model(java.lang.Object)
+     */
+    public IModel<Object> model(final Object object) {
+        return new CompoundPropertyModel<Object>(object) {
+            @Override
+            public boolean equals(final Object obj) {
+                final Object myObj = getObject();
+                if (myObj != null && obj == null) {
+                    return false;
+                }
+                final Object otherObj = ((IModel) obj).getObject();
+                if (myObj == null) {
+                    return otherObj==null;
+                } else {
+                    return myObj.equals(otherObj);
+                }
+            }
 
-			@Override
-			public int hashCode() {
-				return getObject().hashCode();
-			}
-		};
-	}
+            @Override
+            public int hashCode() {
+                return getObject().hashCode();
+            }
+        };
+    }
 
 }
