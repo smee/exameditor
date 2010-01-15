fb2811427fb7248edd7ed17aeb767c85abd6736c
Steffen Dienst <steffen.dienst@gmail.com>
2010-01-15 23:40:31 +0100

    [fix] bug in UserAdminPage, added workaround. BEWARE: The patched page can only add users with role USER, not Admin!

--- exameditor/web/src/main/java/de/elateportal/editor/pages/OverviewPage.java
+++ exameditor/web/src/main/java/de/elateportal/editor/pages/OverviewPage.java
@@ -32,94 +32,101 @@ import de.elateportal.model.TextSubTaskDef;
 
 public class OverviewPage extends WebPage {
 
-  /** Add components to be displayed on page. */
-  public OverviewPage() {
-    if (AuthDataSession.get().isSignedIn()) {
-      add(new ChromeMenu("menubar", getMenuList(), ChromeMenu.Theme.THEME5));
-    } else {
-      add(new NullPlug("menubar"));
-    }
+	/** Add components to be displayed on page. */
+	public OverviewPage() {
+		if (AuthDataSession.get().isSignedIn()) {
+			add(new ChromeMenu("menubar", getMenuList(), ChromeMenu.Theme.THEME5));
+		} else {
+			add(new NullPlug("menubar"));
+		}
 
-    // make sure tinymce works, even when adding it via ajax
-    // see http://wicketbyexample.com/wicket-tinymce-some-advanced-tips/
-    add(new HeaderContributor(new IHeaderContributor() {
-      public void renderHead(final IHeaderResponse response) {
-        response.renderJavascriptReference(TinyMCESettings.javaScriptReference());
-      }
-    }));
+		// make sure tinymce works, even when adding it via ajax
+		// see http://wicketbyexample.com/wicket-tinymce-some-advanced-tips/
+		add(new HeaderContributor(new IHeaderContributor() {
+			public void renderHead(final IHeaderResponse response) {
+				response.renderJavascriptReference(TinyMCESettings.javaScriptReference());
+			}
+		}));
 
-    // sign in/out links
-    add(new DataUserStatusPanel("userStatus") {
-      @Override
-      protected Link getSignInLink(final String id) {
-        return new Link(id) {
-          @Override
-          public boolean isVisible() {
-            // return !getAuthSession().isSignedIn();
-            return false;
-          }
+		// sign in/out links
+		add(new DataUserStatusPanel("userStatus") {
+			@Override
+			protected Class<? extends WebPage> adminPageClass() {
+				return PatchedAdminPage.class;
+			}
 
-          @Override
-          public void onClick() {
-            setResponsePage(new DataSignInPage(new DataSignInPage.ReturnPage() {
-              public Page get() {
-                return new OverviewPage();
-              }
-            }));
-          }
-        };
-      }
-    });
-    add(new DataSignInPanel<BasicUser>("loginpanel", new ReturnPage() {
-      public Page get() {
-        return new OverviewPage();
-      }
-    }) {
-      @Override
-      public boolean isVisible() {
-        return !AuthDataSession.get().isSignedIn();
-      }
-    });
-    add(new Footer("footer"));
-  }
+			@Override
+			protected Link getSignInLink(final String id) {
+				return new Link(id) {
+					@Override
+					public boolean isVisible() {
+						// return !getAuthSession().isSignedIn();
+						return false;
+					}
 
-  /**
-   * @return
-   */
-  private List<List<LinkVO>> getMenuList() {
-    final Class<? extends Page> pageClass = getPage().getClass();
-    final List<List<LinkVO>> res = new LinkedList<List<LinkVO>>();
-    // must not create the link result page now,
-    // leads to StackoverflowError in case of resultpage extends OverviewPage
+					@Override
+					public void onClick() {
+						setResponsePage(new DataSignInPage(new DataSignInPage.ReturnPage() {
+							public Page get() {
+								return new OverviewPage();
+							}
+						}));
+					}
+				};
+			}
+		});
+		add(new DataSignInPanel<BasicUser>("loginpanel", new ReturnPage() {
+			public Page get() {
+				return new OverviewPage();
+			}
+		}) {
+			@Override
+			public boolean isVisible() {
+				return !AuthDataSession.get().isSignedIn();
+			}
+		});
+		add(new Footer("footer"));
+	}
 
-    res.add(Arrays.asList(new LinkVO(TaskDefPage.class, "Prüfungen").setSelected(pageClass.equals(TaskDefPage.class))));
-    res.add(Arrays.asList(new LinkVO(ShowSubtaskDefsPage.class, "Alle Aufgaben").setSelected(pageClass.equals(ShowSubtaskDefsPage.class)), new LinkVO(new Create() {
-      public WebPage createPage() {
-        return new ShowSubtaskDefsPage(McSubTaskDef.class);
-      }
-    }, "Alle MC-Aufgaben"), new LinkVO(new Create() {
+	/**
+	 * @return
+	 */
+	private List<List<LinkVO>> getMenuList() {
+		final Class<? extends Page> pageClass = getPage().getClass();
+		final List<List<LinkVO>> res = new LinkedList<List<LinkVO>>();
+		// must not create the link result page now,
+		// leads to StackoverflowError in case of resultpage extends OverviewPage
 
-      public WebPage createPage() {
-        return new ShowSubtaskDefsPage(ClozeSubTaskDef.class);
-      }
-    }, "Alle Lückentext-Aufgaben"), new LinkVO(new Create() {
+		res.add(Arrays.asList(new LinkVO(TaskDefPage.class, "Prüfungen").setSelected(pageClass.equals(TaskDefPage.class))));
+		res.add(Arrays.asList(new LinkVO(ShowSubtaskDefsPage.class, "Alle Aufgaben").setSelected(pageClass
+		    .equals(ShowSubtaskDefsPage.class)), new LinkVO(new Create() {
+			public WebPage createPage() {
+				return new ShowSubtaskDefsPage(McSubTaskDef.class);
+			}
+		}, "Alle MC-Aufgaben"), new LinkVO(new Create() {
 
-      public WebPage createPage() {
-        return new ShowSubtaskDefsPage(TextSubTaskDef.class);
-      }
-    }, "Alle Text-Aufgaben"), new LinkVO(new Create() {
+			public WebPage createPage() {
+				return new ShowSubtaskDefsPage(ClozeSubTaskDef.class);
+			}
+		}, "Alle Lückentext-Aufgaben"), new LinkVO(new Create() {
 
-      public WebPage createPage() {
-        return new ShowSubtaskDefsPage(PaintSubTaskDef.class);
-      }
-    }, "Alle Zeichen-Aufgaben"), new LinkVO(new Create() {
+			public WebPage createPage() {
+				return new ShowSubtaskDefsPage(TextSubTaskDef.class);
+			}
+		}, "Alle Text-Aufgaben"), new LinkVO(new Create() {
 
-      public WebPage createPage() {
-        return new ShowSubtaskDefsPage(MappingSubTaskDef.class);
-      }
-    }, "Alle Zuordnungs-Aufgaben")));
-    res.add(Arrays.asList(new LinkVO(StatisticPage.class, "Statistiken").setSelected(pageClass.equals(StatisticPage.class))));
-    res.add(Arrays.asList(new LinkVO(UploadComplexTaskdefPage.class, "Importieren").setSelected(pageClass.equals(UploadComplexTaskdefPage.class))));
-    return res;
-  }
+			public WebPage createPage() {
+				return new ShowSubtaskDefsPage(PaintSubTaskDef.class);
+			}
+		}, "Alle Zeichen-Aufgaben"), new LinkVO(new Create() {
+
+			public WebPage createPage() {
+				return new ShowSubtaskDefsPage(MappingSubTaskDef.class);
+			}
+		}, "Alle Zuordnungs-Aufgaben")));
+		res.add(Arrays.asList(new LinkVO(StatisticPage.class, "Statistiken").setSelected(pageClass.equals(StatisticPage.class))));
+		res.add(Arrays.asList(new LinkVO(UploadComplexTaskdefPage.class, "Importieren").setSelected(pageClass
+		    .equals(UploadComplexTaskdefPage.class))));
+		return res;
+	}
 }
