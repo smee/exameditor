5fb964acc4e9e9a09d31dfc0ad2c9c729cef2383
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2010-01-15 14:39:52 +0100

    [feature] added footer with info about number of currently active users

--- exameditor/web/src/main/java/de/elateportal/editor/pages/OverviewPage.java
+++ exameditor/web/src/main/java/de/elateportal/editor/pages/OverviewPage.java
@@ -20,6 +20,7 @@ import wicket.contrib.tinymce.settings.TinyMCESettings;
 import de.elateportal.editor.components.menu.ChromeMenu;
 import de.elateportal.editor.components.menu.LinkVO;
 import de.elateportal.editor.components.menu.LinkVO.Create;
+import de.elateportal.editor.components.panels.Footer;
 import de.elateportal.model.ClozeSubTaskDef;
 import de.elateportal.model.MappingSubTaskDef;
 import de.elateportal.model.McSubTaskDef;
@@ -28,80 +29,82 @@ import de.elateportal.model.TextSubTaskDef;
 
 public class OverviewPage extends WebPage {
 
-	/** Add components to be displayed on page. */
-	public OverviewPage() {
-		if (AuthDataSession.get().isSignedIn())
-			add(new ChromeMenu("menubar", getMenuList(), ChromeMenu.Theme.THEME1));
-		else
-			add(new NullPlug("menubar"));
+  /** Add components to be displayed on page. */
+  public OverviewPage() {
+    if (AuthDataSession.get().isSignedIn()) {
+      add(new ChromeMenu("menubar", getMenuList(), ChromeMenu.Theme.THEME1));
+    } else {
+      add(new NullPlug("menubar"));
+    }
 
-		// make sure tinymce works, even when adding it via ajax
-		// see http://wicketbyexample.com/wicket-tinymce-some-advanced-tips/
-		add(new HeaderContributor(new IHeaderContributor() {
-			public void renderHead(IHeaderResponse response) {
-				response.renderJavascriptReference(TinyMCESettings.javaScriptReference());
-			}
-		}));
+    // make sure tinymce works, even when adding it via ajax
+    // see http://wicketbyexample.com/wicket-tinymce-some-advanced-tips/
+    add(new HeaderContributor(new IHeaderContributor() {
+      public void renderHead(final IHeaderResponse response) {
+        response.renderJavascriptReference(TinyMCESettings.javaScriptReference());
+      }
+    }));
 
-		// sign in/out links
-		add(new DataUserStatusPanel("userStatus") {
-			@Override
-			protected Link getSignInLink(String id) {
-				return new Link(id) {
-					@Override
-					public boolean isVisible() {
-						return !getAuthSession().isSignedIn();
-					}
+    // sign in/out links
+    add(new DataUserStatusPanel("userStatus") {
+      @Override
+      protected Link getSignInLink(final String id) {
+        return new Link(id) {
+          @Override
+          public boolean isVisible() {
+            return !getAuthSession().isSignedIn();
+          }
 
-					@Override
-					public void onClick() {
-						setResponsePage(new DataSignInPage(new DataSignInPage.ReturnPage() {
-							public Page get() {
-								return new OverviewPage();
-							}
-						}));
-					}
-				};
-			}
-		});
-	}
+          @Override
+          public void onClick() {
+            setResponsePage(new DataSignInPage(new DataSignInPage.ReturnPage() {
+              public Page get() {
+                return new OverviewPage();
+              }
+            }));
+          }
+        };
+      }
+    });
+    add(new Footer("footer"));
+  }
 
-	/**
-	 * @return
-	 */
-	private List<List<LinkVO>> getMenuList() {
-		final List<List<LinkVO>> res = new LinkedList<List<LinkVO>>();
-		// must not create the link result page now,
-		// leads to StackoverflowError in case of resultpage extends OverviewPage
+  /**
+   * @return
+   */
+  private List<List<LinkVO>> getMenuList() {
+    final List<List<LinkVO>> res = new LinkedList<List<LinkVO>>();
+    // must not create the link result page now,
+    // leads to StackoverflowError in case of resultpage extends OverviewPage
 
-		res.add(Arrays.asList(new LinkVO(TaskDefPage.class, "Prüfungen")));
-		res.add(Arrays.asList(new LinkVO(ShowSubtaskDefsPage.class, "Alle Aufgaben"), new LinkVO(new Create() {
-			public WebPage createPage() {
-				return new ShowSubtaskDefsPage(McSubTaskDef.class);
-			}
-		}, "Alle MC-Aufgaben"), new LinkVO(new Create() {
+    res.add(Arrays.asList(new LinkVO(TaskDefPage.class, "Prüfungen")));
+    res.add(Arrays.asList(new LinkVO(ShowSubtaskDefsPage.class, "Alle Aufgaben"), new LinkVO(new Create() {
+      public WebPage createPage() {
+        return new ShowSubtaskDefsPage(McSubTaskDef.class);
+      }
+    }, "Alle MC-Aufgaben"), new LinkVO(new Create() {
 
-			public WebPage createPage() {
-				return new ShowSubtaskDefsPage(ClozeSubTaskDef.class);
-			}
-		}, "Alle Lückentext-Aufgaben"), new LinkVO(new Create() {
+      public WebPage createPage() {
+        return new ShowSubtaskDefsPage(ClozeSubTaskDef.class);
+      }
+    }, "Alle Lückentext-Aufgaben"), new LinkVO(new Create() {
 
-			public WebPage createPage() {
-				return new ShowSubtaskDefsPage(TextSubTaskDef.class);
-			}
-		}, "Alle Text-Aufgaben"), new LinkVO(new Create() {
+      public WebPage createPage() {
+        return new ShowSubtaskDefsPage(TextSubTaskDef.class);
+      }
+    }, "Alle Text-Aufgaben"), new LinkVO(new Create() {
 
-			public WebPage createPage() {
-				return new ShowSubtaskDefsPage(PaintSubTaskDef.class);
-			}
-		}, "Alle Zeichen-Aufgaben"), new LinkVO(new Create() {
+      public WebPage createPage() {
+        return new ShowSubtaskDefsPage(PaintSubTaskDef.class);
+      }
+    }, "Alle Zeichen-Aufgaben"), new LinkVO(new Create() {
 
-			public WebPage createPage() {
-				return new ShowSubtaskDefsPage(MappingSubTaskDef.class);
-			}
-		}, "Alle Zuordnungs-Aufgaben")));
-		res.add(Arrays.asList(new LinkVO(StatisticPage.class, "Statistiken")));
-		res.add(Arrays.asList(new LinkVO(UploadComplexTaskdefPage.class, "Importieren")));
-		return res;
-	}
+      public WebPage createPage() {
+        return new ShowSubtaskDefsPage(MappingSubTaskDef.class);
+      }
+    }, "Alle Zuordnungs-Aufgaben")));
+    res.add(Arrays.asList(new LinkVO(StatisticPage.class, "Statistiken")));
+    res.add(Arrays.asList(new LinkVO(UploadComplexTaskdefPage.class, "Importieren")));
+    return res;
+  }
 }
