8ae3cb6f3771cd4157d52cf17a6b3d18e4361c54
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2010-01-18 12:13:39 +0100

    [fix] fixed treenode's model hashcode/equals methods. This leads to nodes that keep their expanded/collapsed state upon rerendering.

--- exameditor/web/src/main/java/de/elateportal/editor/components/panels/tasks/CategoryPanel.java
+++ exameditor/web/src/main/java/de/elateportal/editor/components/panels/tasks/CategoryPanel.java
@@ -2,9 +2,7 @@ package de.elateportal.editor.components.panels.tasks;
 
 import net.databinder.models.hib.HibernateObjectModel;
 
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
-import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -16,60 +14,54 @@ import de.elateportal.model.Category;
  * @author sdienst
  */
 public class CategoryPanel extends Panel {
-	private class CategoryForm extends ShinyForm<Category> {
+  private class CategoryForm extends ShinyForm<Category> {
 
-		private FeedbackPanel feedback;
+    private FeedbackPanel feedback;
 
-		public CategoryForm(final String id) {
-			super(id, Category.class);
-			init();
-		}
+    public CategoryForm(final String id) {
+      super(id, Category.class);
+      init();
+    }
 
-		public CategoryForm(final String id, final HibernateObjectModel<Category> hibernateObjectModel) {
-			super(id, hibernateObjectModel);
-			init();
-		}
+    public CategoryForm(final String id, final HibernateObjectModel<Category> hibernateObjectModel) {
+      super(id, hibernateObjectModel);
+      init();
+    }
 
-		private void init() {
-			add(feedback = new FeedbackPanel("feedback"));
-			feedback.setOutputMarkupId(true);
-			add(new TextField<String>("id").setRequired(true));
-			add(new TextField<String>("title").setRequired(true));
-			add(new TextField<Integer>("tasksPerPage"));
+    private void init() {
+      add(feedback = new FeedbackPanel("feedback"));
+      feedback.setOutputMarkupId(true);
+      add(new TextField<String>("id").setRequired(true));
+      add(new TextField<String>("title").setRequired(true));
+      add(new TextField<Integer>("tasksPerPage"));
 
-			add(new AjaxButton("saveButton") {
+      add(new Button("saveButton") {
 
-				@Override
-				protected void onError(AjaxRequestTarget target, Form<?> form) {
-					super.onError(target, form);
-					target.addComponent(feedback);
-				}
+        @Override
+        public void onSubmit() {
+          super.onSubmit();
+          info("Gespeichert!");
+        }
 
-				@Override
-				protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-					feedback.info("Gespeichert!");
-					target.addComponent(feedback);
-				}
+      });
+      add(new org.apache.wicket.markup.html.form.Button("cancelButton") {
+        @Override
+        public void onSubmit() {
+          clearPersistentObject();
+        }
+      }.setDefaultFormProcessing(false));
+    }
 
-			});
-			add(new org.apache.wicket.markup.html.form.Button("cancelButton") {
-				@Override
-				public void onSubmit() {
-					clearPersistentObject();
-				}
-			}.setDefaultFormProcessing(false));
-		}
+  }
 
-	}
+  public CategoryPanel(final String id) {
+    super(id);
+    add(new CategoryForm(id));
+  }
 
-	public CategoryPanel(final String id) {
-		super(id);
-		add(new CategoryForm(id));
-	}
-
-	public CategoryPanel(final String id, final HibernateObjectModel<Category> model) {
-		super(id, model);
-		add(new CategoryForm("categoryform", model));
-	}
+  public CategoryPanel(final String id, final HibernateObjectModel<Category> model) {
+    super(id, model);
+    add(new CategoryForm("categoryform", model));
+  }
 
 }
--- exameditor/web/src/main/java/de/elateportal/editor/components/panels/tree/ComplexTaskdefTreeProvider.java
+++ exameditor/web/src/main/java/de/elateportal/editor/components/panels/tree/ComplexTaskdefTreeProvider.java
@@ -18,10 +18,13 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package de.elateportal.editor.components.panels.tree;
 
+import java.io.Serializable;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.wicket.model.CompoundPropertyModel;
+import net.databinder.models.hib.HibernateObjectModel;
+
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.wicket.model.IModel;
 import org.jvnet.hyperjaxb3.item.Item;
 
@@ -45,125 +48,147 @@ import de.elateportal.model.Category.TextTaskBlock;
  * 
  */
 public class ComplexTaskdefTreeProvider implements ITreeProvider<Object> {
-	final static Function<Item, Object> itemGetter = new Function<Item, Object>() {
+  final static Function<Item, Object> itemGetter = new Function<Item, Object>() {
 
-		public Object apply(final Item item) {
-			return item.getItem();
-		}
-	};
+    public Object apply(final Item item) {
+      return item.getItem();
+    }
+  };
 
-	private final IModel<List<ComplexTaskDef>> model;
+  private final IModel<List<ComplexTaskDef>> model;
 
-	public ComplexTaskdefTreeProvider(final IModel<List<ComplexTaskDef>> model) {
-		this.model = model;
-	}
+  public ComplexTaskdefTreeProvider(final IModel<List<ComplexTaskDef>> model) {
+    this.model = model;
+  }
 
-	public IModel createLabelModel(final IModel<?> model) {
-		return model;
-	}
+  public IModel createLabelModel(final IModel<?> model) {
+    return model;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.wicket.model.IDetachable#detach()
-	 */
-	public void detach() {
-		model.detach();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.apache.wicket.model.IDetachable#detach()
+   */
+  public void detach() {
+    model.detach();
+  }
 
-	private Iterator<TaskBlockType> getChildren(final Category cat) {
-		return Iterators.transform(cat.getMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItems().iterator(),
-		    new Function<CategoryMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItem, TaskBlockType>() {
+  private Iterator<TaskBlockType> getChildren(final Category cat) {
+    return Iterators.transform(cat.getMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItems().iterator(),
+        new Function<CategoryMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItem, TaskBlockType>() {
 
-			public TaskBlockType apply(final CategoryMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItem block) {
-				return block.getItem();
-			}
-		});
-	}
+      public TaskBlockType apply(final CategoryMcTaskBlockOrClozeTaskBlockOrTextTaskBlockItem block) {
+        return block.getItem();
+      }
+    });
+  }
 
-	private Iterator<Object> getChildren(final ClozeTaskBlock tb) {
-		return Iterators.transform(tb.getClozeSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+  private Iterator<Object> getChildren(final ClozeTaskBlock tb) {
+    return Iterators.transform(tb.getClozeSubTaskDefOrChoiceItems().iterator(), itemGetter);
+  }
 
-	private Iterator<Category> getChildren(final ComplexTaskDef ctd) {
-		return ctd.getCategory().iterator();
-	}
+  private Iterator<Category> getChildren(final ComplexTaskDef ctd) {
+    return ctd.getCategory().iterator();
+  }
 
-	private Iterator<Object> getChildren(final MappingTaskBlock tb) {
-		return Iterators.transform(tb.getMappingSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+  private Iterator<Object> getChildren(final MappingTaskBlock tb) {
+    return Iterators.transform(tb.getMappingSubTaskDefOrChoiceItems().iterator(), itemGetter);
+  }
 
-	private Iterator<Object> getChildren(final McTaskBlock tb) {
-		return Iterators.transform(tb.getMcSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+  private Iterator<Object> getChildren(final McTaskBlock tb) {
+    return Iterators.transform(tb.getMcSubTaskDefOrChoiceItems().iterator(), itemGetter);
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see wickettree.ITreeProvider#getChildren(java.lang.Object)
-	 */
-	public Iterator<? extends Object> getChildren(final Object object) {
-		if (object instanceof ComplexTaskDef) {
-			return getChildren((ComplexTaskDef) object);
-		} else if (object instanceof Category) {
-			return getChildren((Category) object);
-		} else if (object instanceof McTaskBlock) {
-			return getChildren((McTaskBlock) object);
-		} else if (object instanceof ClozeTaskBlock) {
-			return getChildren((ClozeTaskBlock) object);
-		} else if (object instanceof MappingTaskBlock) {
-			return getChildren((MappingTaskBlock) object);
-		} else if (object instanceof TextTaskBlock) {
-			return getChildren((TextTaskBlock) object);
-		} else if (object instanceof PaintTaskBlock) {
-			return getChildren((PaintTaskBlock) object);
-		} else {
-			return Iterators.emptyIterator();
-		}
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see wickettree.ITreeProvider#getChildren(java.lang.Object)
+   */
+  public Iterator<? extends Object> getChildren(final Object object) {
+    if (object instanceof ComplexTaskDef) {
+      return getChildren((ComplexTaskDef) object);
+    } else if (object instanceof Category) {
+      return getChildren((Category) object);
+    } else if (object instanceof McTaskBlock) {
+      return getChildren((McTaskBlock) object);
+    } else if (object instanceof ClozeTaskBlock) {
+      return getChildren((ClozeTaskBlock) object);
+    } else if (object instanceof MappingTaskBlock) {
+      return getChildren((MappingTaskBlock) object);
+    } else if (object instanceof TextTaskBlock) {
+      return getChildren((TextTaskBlock) object);
+    } else if (object instanceof PaintTaskBlock) {
+      return getChildren((PaintTaskBlock) object);
+    } else {
+      return Iterators.emptyIterator();
+    }
+  }
 
-	private Iterator<Object> getChildren(final PaintTaskBlock tb) {
-		return Iterators.transform(tb.getPaintSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+  private Iterator<Object> getChildren(final PaintTaskBlock tb) {
+    return Iterators.transform(tb.getPaintSubTaskDefOrChoiceItems().iterator(), itemGetter);
+  }
 
-	private Iterator<Object> getChildren(final TextTaskBlock tb) {
-		return Iterators.transform(tb.getTextSubTaskDefOrChoiceItems().iterator(), itemGetter);
-	}
+  private Iterator<Object> getChildren(final TextTaskBlock tb) {
+    return Iterators.transform(tb.getTextSubTaskDefOrChoiceItems().iterator(), itemGetter);
+  }
 
-	public Iterator<? extends Object> getRoots() {
-		return model.getObject().iterator();
-	}
+  public Iterator<? extends Object> getRoots() {
+    return model.getObject().iterator();
+  }
 
-	public boolean hasChildren(final Object object) {
-		return Iterators.size(getChildren(object)) > 0;
-	}
+  public boolean hasChildren(final Object object) {
+    return Iterators.size(getChildren(object)) > 0;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see wickettree.ITreeProvider#model(java.lang.Object)
-	 */
-	public IModel<Object> model(final Object object) {
-		return new CompoundPropertyModel<Object>(object) {
-			@Override
-			public boolean equals(final Object obj) {
-				final Object myObj = getObject();
-				if (myObj != null && obj == null) {
-					return false;
-				}
-				final Object otherObj = ((IModel) obj).getObject();
-				if (myObj == null) {
-					return otherObj == null;
-				} else {
-					return myObj.equals(otherObj);
-				}
-			}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see wickettree.ITreeProvider#model(java.lang.Object)
+   */
+  @SuppressWarnings("unchecked")
+  public IModel<Object> model(final Object object) {
+    try {
+      return new HibernateObjectModel(object.getClass(), (Serializable) PropertyUtils.getProperty(object, "hjid")) {
+        @Override
+        public boolean equals(final Object obj) {
+          final Object target = getObject();
+          if (target != null && obj instanceof HibernateObjectModel) {
+            return equalId(target, ((HibernateObjectModel) obj).getObject());
+          }
+          return super.equals(obj);
+        }
 
-			@Override
-			public int hashCode() {
-				return getObject().hashCode();
-			}
-		};
-	}
+        private boolean equalId(final Object o1, final Object o2) {
+          return o1.getClass().equals(o2.getClass()) && getId(o1).equals(getId(o2));
+        }
+
+        private Object getId(final Object o) {
+          try {
+            return PropertyUtils.getProperty(o, "hjid");
+          } catch (final Exception e) {
+            e.printStackTrace();
+            return o.toString();
+          }
+        }
+
+        @Override
+        public int hashCode() {
+          final Object target = getObject();
+          if (target == null) {
+            return super.hashCode();
+          }
+          int hash = 1;
+          hash = hash * 31 + target.getClass().hashCode();
+          hash = hash * 31 + getId(target).hashCode();
+          return hash;
+
+        }
+      };
+    } catch (final Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 
 }
