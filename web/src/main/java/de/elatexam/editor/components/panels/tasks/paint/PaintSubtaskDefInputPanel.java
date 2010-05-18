220a1c6a0d74712ba0c13f02632e377afe827863
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2010-05-18 11:00:42 +0200

    interim checkin

--- web/src/main/java/de/elatexam/editor/components/panels/tasks/paint/PaintSubtaskDefInputPanel.java
+++ web/src/main/java/de/elatexam/editor/components/panels/tasks/paint/PaintSubtaskDefInputPanel.java
@@ -1,8 +1,11 @@
 package de.elatexam.editor.components.panels.tasks.paint;
 
 import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.HiddenField;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.validation.validator.MinimumValidator;
 
 import com.mrhaki.wicket.components.DeployJava;
@@ -16,7 +19,7 @@ public class PaintSubtaskDefInputPanel extends SubtaskSpecificsInputPanel<PaintS
   private final DeployJava applet;
 
   public PaintSubtaskDefInputPanel(final String id, final IModel<PaintSubTaskDef> model) {
-        super(id);
+        super(id,model);
 
     applet = new DeployJava("paintapplet");
     applet.setHeight(400);
@@ -35,6 +38,19 @@ public class PaintSubtaskDefInputPanel extends SubtaskSpecificsInputPanel<PaintS
     add(new TextField("textualAnswer.textFieldHeight").add(new MinimumValidator<Integer>(0)));
     add(new CheckBox("colorChangeable"));
     add(new CheckBox("strokewidthChangeable"));
+
+    add(new HiddenField<String>("hiddenBGImage", Model.of("foo")));
+    add(new Link<String>("storeImage") {
+      @Override
+      protected CharSequence getOnClickScript(final CharSequence url) {
+        return "document.getElementById('taskform12').hiddenBGImage.value=document.taskform.paintapplet.getForegroundPictureWithUndoData(5);return false;";
+      }
+      @Override
+      public void onClick() {
+
+      }
+
+    });
     }
 
 }
